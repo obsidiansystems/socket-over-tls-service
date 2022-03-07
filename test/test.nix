7786ddf4207e94c6083abc34ea627733925f5062
Rune K. Svendsen <runesvend@gmail.com>
2022-03-07 09:57:39 +0100

    test.nix: fix client.nix path

--- test/test.nix
+++ test/test.nix
@@ -2,7 +2,7 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/0f8f64b54ed07966b83db2f20c888d5e035012ef.tar.gz";
   pkgs = import nixpkgs {};
-  client = import ./client.nix {pkgs = pkgs; serverCertFile = serverCertFile;};
+  client = import ../client.nix {pkgs = pkgs; serverCertFile = serverCertFile;};
 
   port = 9186;
   serverSocketFile = "/home/socket-forward/forward.socket";
